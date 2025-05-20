c64ab39470abc6b52785e34766300760dafd29de
786fawad <fawad.rehmans@gmail.com>
2025-05-20 17:40:49 -0400

    Update main.py gpt model

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def get_openai_message():
     )
 
     completion = openai.ChatCompletion.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo,
         messages=[{"role": "user", "content": prompt}]
     )
 
